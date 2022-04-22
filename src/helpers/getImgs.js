d896ae72a5cc30ce8bf8a43743e022528440d89a
Andres Castro <andrescastro2011@hotmail.com>
2022-04-22 17:07:27 -0300

    solucion en el error 401 api

--- src/helpers/getImgs.js
+++ src/helpers/getImgs.js
@@ -1,9 +1,16 @@
 
-export const getImgs = async ( category) => {
+export const getImgs = async ( category ) => {
       
-    const url = `https://api.pexels.com/v1/search?query=${encodeURI(category)}&orientation:portrait&api_key=563492ad6f91700001000001cd67dbb509844c9585ba25ee34ce37fb`
 
-    const respuesta = await fetch(url);
+    const apikey = '563492ad6f91700001000001cd67dbb509844c9585ba25ee34ce37fb';
+    const url = `https://api.pexels.com/v1/search?query=${encodeURI(category)}&api_key=${apikey}`;
+
+    const respuesta = await fetch(url ,{
+        method : 'GET',
+        headers :{
+            Authorization : `${apikey}`
+        }
+    });
 
     const {photos} = await respuesta.json();
 
